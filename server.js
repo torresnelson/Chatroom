ff529b3fae59e6f5798b7ad2efbd6f89936ab400
Nelson Torres <40880521+torresnelson@users.noreply.github.com>
2021-06-25 10:01:09 -0300

    Add files

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+ 
+app.get('/', (req, res) => {
+	res.sendFile(`${__dirname}/index.html`);
+});
+ 
+io.on('connection', socket => {
+	console.log('socket connected!');
+	socket.on('disconnect', () => {
+       console.log(`${socket.name} disconnected!`);
+       io.emit('leave', socket.name);
+	});
+ 
+   	socket.on('join', name => {
+		console.log(`${name} joined!`);
+		socket.name = name;
+		io.emit('join', name);
+	});
+
+	socket.on('chat', data => {
+		console.log(`chat data: ${JSON.stringify(data, null, 2)}`);
+		io.emit('chat', data);
+	});
+
+	socket.on('start-type', (name, cb) => {
+		console.log(`${name} started to type`);
+		socket.broadcast.emit('start-type', name);
+		cb(); //funcion de que el cliente lo reconoce
+	});
+	socket.on('stop-type', (name, cb) => {
+		console.log(`${name} stopped typing`);
+		socket.broadcast.emit('stop-type', name);
+		cb();
+	});
+});
+
+ 
+http.listen(8000, () => {
+	console.log('> http://localhost:8000');
+});
